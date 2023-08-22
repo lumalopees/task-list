0e876b595bb42fdba57e6cf1302283ace309ee12
Luma <luuh_lopees@live.com>
2023-08-21 23:27:54 -0300

    dom names and add events

--- javascript.js
+++ javascript.js
@@ -3,3 +3,17 @@ const taskInput = document.getElementById("wu");
 const taskList = document.getElementById("task-list");
 const deleteCompletedBtn = document.getElementById("delete-completed");
 
+taskForm.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  const taskText = taskInput.value.trim(); 
+  if (taskText !== "") { 
+    const newTask = document.createElement("li");
+    newTask.innerHTML = `
+      <input type="checkbox" class="task-checkbox">
+      <span>${taskText}</span>
+    `;
+    taskList.appendChild(newTask); 
+    taskInput.value = "";
+  }
+});
+
