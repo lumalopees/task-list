4513aebf7c51d3c1c0f178027fb210f17fdf3b0d
Luma <luuh_lopees@live.com>
2023-08-21 23:14:18 -0300

    add: dom elements

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+const deleteCompletedBtn = document.getElementById("delete-completed");
+
