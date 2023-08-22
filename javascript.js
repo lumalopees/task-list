0d2f7504e2c185aa5df09335559d577b1b4f066f
Luma <luuh_lopees@live.com>
2023-08-21 23:21:38 -0300

    deleted button

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const taskForm = document.getElementById("task-form");
-const taskInput = document.getElementById("task-input");
+const taskInput = document.getElementById("wu");
 const taskList = document.getElementById("task-list");
 const deleteCompletedBtn = document.getElementById("delete-completed");
 
